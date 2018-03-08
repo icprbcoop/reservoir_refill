3346a693a4af38361bd6cfeccbba8d0bc85b5dcf
Alimatou Seck <alimatouseck@Alimatous-Air.home>
2018-03-07 22:39:12 -0500

    clean up

--- app.R
+++ app.R
@@ -7,6 +7,7 @@ library(data.table)
 library(ggplot2)
 
 
+#functions###########################################################
 numberOfDays <- function(date) {
   m <- format(date, format="%m")
   
@@ -18,6 +19,7 @@ numberOfDays <- function(date) {
 }
 
 
+#Shiny user interface ###############################################
 
 ui <- dashboardPage(
   dashboardHeader(title = "Basic dashboard"),
@@ -41,9 +43,12 @@ ui <- dashboardPage(
   )
 )
 
+#Shiny server #######################################################
+
 server <- function(input, output) {
   output$plot1 <- renderPlot({
     
+    #inputs ########################################################
     #todayis <- as.Date("2016-10-5", "%Y-%m-%d")  #input variable (to be)
     todayis <- input$date_input
     startstorage <- input$beginning_storage                        #input variable (to be)
@@ -70,12 +75,7 @@ server <- function(input, output) {
     monthinflow3.df <- setDT(monthinflow2.df)[, lapply(.SD, function(x) first(na.omit(x))), by = Month]
     monthinflow4.df <- monthinflow3.df %>%
       slice(match(month_order, Month))
-    #group_by(Month) #%>% 
-    #summarise_all(funs(na.omit))
-    #slice(1:12)
-    #distinct(Month, .keep_all = TRUE)
     
-    #if {month(todaysi)}
     #how many days in month and how many days left
     ndays_todayis <- numberOfDays(todayis)
     rdays_todayis <- numberOfDays(todayis) - day(todayis)
@@ -86,13 +86,15 @@ server <- function(input, output) {
     a.df <- startstorage - b.df
     c.df <- monthinflow4.df * ratiodays_todaysis / 1000
     d.df <- a.df$V2 + c.df
+    d.df <- replace(d.df, d.df > capacity, capacity)
+    d.df <- replace(d.df, d.df < deadstorage, deadstorage)
     d.df$Month <- monthinflow4.df$Month
     d.df$MonthIndex <- c(6,7,8,9,10,11,12,13,14,15,16,17)
-    #d.df$"1930"[df$Month2 < month(todayis)] <- 23
+  
     quants <- c(0.05,0.50,0.9,0.95)
     percentile.df <- apply( d.df[2:81] , 1 , quantile , probs = quants , na.rm = TRUE )
     colnames(percentile.df) <- c("June", "July", "August", "September", "October", "November", "December", "January", "February", "March", "April", "May")
-    percentile5.df <- percentile.df[1, ]
+    #percentile5.df <- percentile.df[1, ]
     
     #Plots###############################################  
     plot(1:12,percentile.df[2,],type="l",ylim=range(percentile.df),xlab="Month",ylab="Storage (BG)")
